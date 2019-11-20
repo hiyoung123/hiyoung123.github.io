f6555056c30b38bb559665e5b664da37a8e2e648
liuhaiyang <liuhaiyang.x@bytedance.com>
2019-11-20 12:56:16 +0800

    Site updated: 2019-11-20 12:56:15

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,3 +1,3 @@
-// build time:Wed Nov 20 2019 12:10:03 GMT+0800 (GMT+08:00)
+// build time:Wed Nov 20 2019 12:56:09 GMT+0800 (GMT+08:00)
 $(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
 //rebuild by neat 
